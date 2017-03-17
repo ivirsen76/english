25a20508f412373354e537a510030f03ef4001cf
Igor Eremeev <ivirsen@gmail.com>
2017-03-17 16:46:04 -0400

    Fix eslint issues

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 /* eslint-disable import/default */
 
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import 'semantic-ui-css/semantic.css';
 import routes from './routes';
 import configureStore from './store/configureStore';
+import './styles/styles.scss';
+
 require('./favicon.ico'); // Tell webpack to load favicon.ico
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import 'semantic-ui-css/semantic.css';
-import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
 
@@ -17,7 +18,7 @@ const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
 render(
-  <Provider store={store}>
-    <Router history={history} routes={routes} />
-  </Provider>, document.getElementById('app')
+    <Provider store={store}>
+        <Router history={history} routes={routes} />
+    </Provider>, document.getElementById('app')
 );
