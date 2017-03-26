4b65b9bfe56374bd4fae0a8eba550b20eba04016
Igor Eremeev <ivirsen@gmail.com>
2017-03-26 13:00:35 -0400

    Add auth reducer to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
+import auth from './auth';
 import card from './card';
 
 const rootReducer = combineReducers({
+    auth,
     card,
     form: formReducer,
     routing: routerReducer,
