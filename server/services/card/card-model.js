9ad65f0e687943229a2080785baf0e99bf12285e
Igor Eremeev <ivirsen@gmail.com>
2017-03-29 17:25:49 -0400

    Add server code to the repo

---
+++ server/services/card/card-model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// card-model.js - A sequelize model
+//
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+
+const Sequelize = require('sequelize');
+
+module.exports = function(sequelize) {
+  const card = sequelize.define('cards', {
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    text: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    translate: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    label: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+  }, {
+    freezeTableName: true,
+  });
+
+  card.sync();
+
+  return card;
+};
---
+++ server/services/card/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const service = require('feathers-sequelize');
+const card = require('./card-model');
+const hooks = require('./hooks');
+
+module.exports = function(){
+  const app = this;
+
+  const options = {
+    Model: card(app.get('sequelize')),
+    paginate: {
+      default: 1000,
+      max: 1000,
+    }
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/cards', service(options));
+
+  // Get our initialize service to that we can bind hooks
+  const cardService = app.service('/cards');
+
+  // Set up our before hooks
+  cardService.before(hooks.before);
+
+  // Set up our after hooks
+  cardService.after(hooks.after);
+};
