048c263d14821ea02cb940337fc45a851a24c398
Igor Eremeev <ivirsen@gmail.com>
2017-04-08 23:12:26 -0400

    Use build files from assets virtual foler

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,7 +30,7 @@ module.exports = {
     target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
     output: {
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
+        publicPath: '/assets',
         filename: '[name].[chunkhash].js',
     },
     plugins: [
