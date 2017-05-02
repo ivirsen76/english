7ab490238a18d4496fefd06664dd32c5efb573f9
Igor Eremeev <ivirsen@gmail.com>
2017-05-02 00:01:30 -0400

    Generate mp3 files and put them to AWS S3

--- server/services/card/hooks/generateMp3.js
+++ server/services/card/hooks/generateMp3.js
@@ -1,5 +1,11 @@
 const AWS = require('aws-sdk')
 const randomstring = require('randomstring')
+const temp = require('temp')
+const fs = require('fs')
+const fsp = require('fs-promise')
+const template = require('string-template')
+const exec = require('child-process-promise').exec
+
 
 AWS.config.update({
     region: process.env.AWS_DEFAULT_REGION,
@@ -12,13 +18,24 @@ const voices = {
     ru: 'Tatyana',
 }
 
+const getTmpFile = content => new Promise((resolve, reject) => {
+    temp.open({ prefix: 'english_', suffix: '.mp3' }, (err, info) => {
+        if (err) throw err
+
+        fs.writeFile(info.path, content, (err1) => {
+            if (err1) throw err1
+            resolve(info.path)
+        })
+    })
+})
+
 const generateMp3 = async (id, userId, text, language) => {
-    const filename = `${userId}/${id}_${language}_${randomstring.generate(6)}.mp3`
     const voice = voices[language]
     if (!voice) {
         throw new Error(`There is no voice for "${language}" language`)
     }
 
+    // Generate mp3 using AWS Polly
     const data = await polly.synthesizeSpeech({
         OutputFormat: 'mp3',
         SampleRate: '22050',
@@ -26,13 +43,41 @@ const generateMp3 = async (id, userId, text, language) => {
         VoiceId: voice,
     }).promise()
 
+    const tmpFilename = await getTmpFile(data.AudioStream)
+    const encodedTmpFilename = tmpFilename + '.mp3'
+
+    // Get the duration
+    const info = await exec(template(process.env.SOUND_GET_MP3_DURATION_COMMAND, { filename: tmpFilename }))
+    const duration = Math.round(/Length[^\d]*([\d.]*)/g.exec(info.stdout)[1] * 1000)
+
+    // Normalize the volume
+    await exec(template(process.env.SOUND_NORMALIZE_COMMAND, { filename: tmpFilename }))
+
+    // Encode with standard bitrate
+    await exec(template(process.env.SOUND_ENCODE_MP3_COMMAND, {
+        filein: tmpFilename,
+        fileout: encodedTmpFilename,
+    }))
+
+    // Write file to the AWS S3 bucket
+    const filename = `${id}_${language}_${randomstring.generate(6)}.mp3`
+    const content = await fsp.readFile(encodedTmpFilename)
     await s3.putObject({
         Bucket: process.env.AWS_S3_BUCKET,
-        Key: `public/${filename}`,
+        Key: `public/${userId}/${filename}`,
         ACL: 'public-read',
-        Body: data.AudioStream,
+        Body: content,
         ContentType: data.ContentType,
     }).promise()
+
+    // Remove temp files
+    await fsp.unlink(tmpFilename)
+    await fsp.unlink(encodedTmpFilename)
+
+    return {
+        filename,
+        duration,
+    }
 }
 
 
@@ -40,11 +85,12 @@ module.exports = options => async (hook) => {
     try {
         const { id, text, translate, userId } = hook.result.dataValues
 
-        await Promise.all([
+        const result = await Promise.all([
             generateMp3(id, userId, text, 'en'),
             generateMp3(id, userId, text, 'us'),
             generateMp3(id, userId, translate, 'ru'),
         ])
+        console.log(result)
 
         console.log('The files have been saved') // eslint-disable-line no-console
     } catch (err) {
