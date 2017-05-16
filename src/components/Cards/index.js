27525f4e42f447b4effe81ebf0c2da75492c2dd5
Igor Eremeev <ivirsen@gmail.com>
2017-05-16 15:48:53 -0400

    Fix test warnings

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -4,55 +4,53 @@ import EditCard from 'components/EditCard'
 import DeleteCard from 'components/DeleteCard'
 import AudioLink from './AudioLink'
 
-
 export const Cards = ({ data, deleteCard, updateCard }) => {
-    const columns = [{
-        name: 'actions',
-        label: '',
-        render(value, row) {
-            return (
-                <div>
-                    <EditCard updateCard={updateCard} initialValues={row} />
-                    <DeleteCard deleteCard={deleteCard} id={row.id} />
-                </div>
-            )
+    const columns = [
+        {
+            name: 'actions',
+            label: '',
+            render(value, row) {
+                return (
+                    <div>
+                        <EditCard updateCard={updateCard} initialValues={row} />
+                        <DeleteCard deleteCard={deleteCard} id={row.id} />
+                    </div>
+                )
+            },
         },
-    }, {
-        name: 'text',
-        label: 'Текст',
-        filter: true,
-        sort: true,
-        render(value, row) {
-            return <AudioLink text={value} audioUrl={row.usSoundFile} />
+        {
+            name: 'text',
+            label: 'Текст',
+            filter: true,
+            sort: true,
+            render(value, row) {
+                return <AudioLink text={value} audioUrl={row.usSoundFile} />
+            },
         },
-    }, {
-        name: 'translate',
-        label: 'Перевод',
-        filter: true,
-        sort: true,
-        render(value, row) {
-            return <AudioLink text={value} audioUrl={row.ruSoundFile} />
+        {
+            name: 'translate',
+            label: 'Перевод',
+            filter: true,
+            sort: true,
+            render(value, row) {
+                return <AudioLink text={value} audioUrl={row.ruSoundFile} />
+            },
         },
-    }, {
-        name: 'label',
-        label: 'Метка',
-        filter: true,
-        sort: true,
-    }]
+        {
+            name: 'label',
+            label: 'Метка',
+            filter: true,
+            sort: true,
+        },
+    ]
 
-    return (
-        <Table
-            data={data}
-            columns={columns}
-            showRowNumber
-        />
-    )
+    return <Table data={data} columns={columns} showRowNumber />
 }
 
 Cards.propTypes = {
     data: PropTypes.array.isRequired,
-    updateCard: PropTypes.func.isRequired,
-    deleteCard: PropTypes.func.isRequired,
+    updateCard: PropTypes.func,
+    deleteCard: PropTypes.func,
 }
 
 export default Cards
