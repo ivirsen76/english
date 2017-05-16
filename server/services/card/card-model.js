ce64179816cefdd23b81cae1342b855a467d36da
Igor Eremeev <ivirsen@gmail.com>
2017-05-15 22:58:21 -0400

    Run test only for changed files

--- server/services/card/card-model.js
+++ server/services/card/card-model.js
@@ -1,38 +1,42 @@
 const Sequelize = require('sequelize')
 
-module.exports = function (sequelize) {
-    const card = sequelize.define('cards', {
-        userId: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
+module.exports = sequelize => {
+    const card = sequelize.define(
+        'cards',
+        {
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            text: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            translate: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            label: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            ukSoundFile: Sequelize.STRING,
+            ukSoundLength: Sequelize.INTEGER,
+            usSoundFile: Sequelize.STRING,
+            usSoundLength: Sequelize.INTEGER,
+            ruSoundFile: Sequelize.STRING,
+            ruSoundLength: Sequelize.INTEGER,
+            status: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            statusUpdatedAt: Sequelize.DATE,
         },
-        text: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        translate: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        label: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        ukSoundFile: Sequelize.STRING,
-        ukSoundLength: Sequelize.INTEGER,
-        usSoundFile: Sequelize.STRING,
-        usSoundLength: Sequelize.INTEGER,
-        ruSoundFile: Sequelize.STRING,
-        ruSoundLength: Sequelize.INTEGER,
-        status: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 0,
-        },
-        statusUpdatedAt: Sequelize.DATE,
-    }, {
-        freezeTableName: true,
-    })
+        {
+            freezeTableName: true,
+        }
+    )
 
     return card
 }
