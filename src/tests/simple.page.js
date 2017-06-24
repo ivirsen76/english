1fb5d8dc8fd76e9c7bc7fbb6ef0ea3896f8d85a8
Igor Eremeev <ivirsen@gmail.com>
2017-06-24 02:06:13 -0400

    Use role in beforeEach

--- src/tests/simple.page.js
+++ src/tests/simple.page.js
@@ -2,10 +2,11 @@
 import { Selector } from 'testcafe'
 import { regularUser } from './roles.js'
 
-fixture('Hello world').page('http://localhost:3000')
+fixture('Hello world').page('http://localhost:3000').beforeEach(async t => {
+    await t.useRole(regularUser)
+})
 
 test('My first test', async t => {
-    await t.useRole(regularUser)
     await t.wait(2000)
     await t.expect(Selector('h2').innerText).eql('Главная')
 })
