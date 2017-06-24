ffa77a9e43b0cb24f294a22f62b4061c193af8d4
Igor Eremeev <ivirsen@gmail.com>
2017-06-24 10:53:01 -0400

    Allow console.info for eslint

--- src/tests/simple.page.js
+++ src/tests/simple.page.js
@@ -2,11 +2,12 @@
 import { Selector } from 'testcafe'
 import { regularUser } from './roles.js'
 
-fixture('Hello world').page('http://localhost:3000').beforeEach(async t => {
+fixture('Hello world').beforeEach(async t => {
     await t.useRole(regularUser)
 })
 
 test('My first test', async t => {
+    await t.page('http://localhost:3000')
     await t.wait(2000)
     await t.expect(Selector('h2').innerText).eql('Главная')
 })
