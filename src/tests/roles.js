5c59b39efdc149219243cbfc49b7824397ee3404
Igor Eremeev <ivirsen@gmail.com>
2017-06-25 02:09:37 -0400

    Rename test file

--- src/tests/roles.js
+++ src/tests/roles.js
@@ -2,8 +2,8 @@ import { Role } from 'testcafe'
 
 export const regularUser = Role('http://localhost:3000/login', async t => {
     await t
-        .typeText('input[name=email]', 'ivirsen@gmail.com')
-        .typeText('input[name=password]', 'password')
+        .typeText('input[name=email]', 'ivirsen@gmail.com', { paste: true })
+        .typeText('input[name=password]', 'password', { paste: true })
         .click('button')
 })
 
