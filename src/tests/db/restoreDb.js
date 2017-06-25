2caaba5cc72e89c04f163e59ee401c546e0d29bb
Igor Eremeev <ivirsen@gmail.com>
2017-06-25 03:28:24 -0400

    Change restore db command

--- src/tests/db/restoreDb.js
+++ src/tests/db/restoreDb.js
@@ -3,9 +3,9 @@ import dbConfig from '../../server/config/default.js'
 import parse from 'parse-db-url'
 
 const db = parse(dbConfig.mysql)
-const command = `mysql -s -u ${db.user} --password=${db.password} ${db.database} < src/tests/db/dump.sql`
+const command = `mysql -u ${db.user} --password=${db.password} ${db.database} < src/tests/db/dump.sql`
+shell.config.silent = true
 
 export default () => {
-    shell.config.silent = true
     shell.exec(command)
 }
