60d63399d12d01a7d24b53717f5a5e1a19e1a526
Igor Eremeev <ivirsen@gmail.com>
2017-07-05 14:22:57 -0400

    Add icons to the menu

--- src/client/components/UserArea/SideMenu/index.js
+++ src/client/components/UserArea/SideMenu/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Menu } from 'semantic-ui-react'
 import MenuLink from 'components/MenuLink'
+import Icon from './Icon'
 
-const SideMenu = ({ cardTotal, cardRememberTotal, cardWriteTotal }) => (
-    <Menu vertical fluid size="large">
+const SideMenu = ({ cardTotal, cardRememberTotal, cardWriteTotal }) =>
+    <Menu vertical fluid size="massive">
         <Menu.Item>
             <Menu.Header>Словарь</Menu.Header>
 
             <Menu.Menu>
                 <MenuLink to="/user/cards">
+                    <Icon type="books" />
                     Мои слова
                     <div className="ui label">{cardTotal}</div>
                 </MenuLink>
@@ -21,14 +23,17 @@ const SideMenu = ({ cardTotal, cardRememberTotal, cardWriteTotal }) => (
 
             <Menu.Menu>
                 <MenuLink to="/user/remember">
+                    <Icon type="bubble2" />
                     Запоминание
                     <div className="ui label">{cardRememberTotal}</div>
                 </MenuLink>
                 <MenuLink to="/user/write">
+                    <Icon type="pencil2" />
                     Написание
                     <div className="ui label">{cardWriteTotal}</div>
                 </MenuLink>
                 <MenuLink to="/user/mp3">
+                    <Icon type="headphones" />
                     Скачать MP3
                 </MenuLink>
             </Menu.Menu>
@@ -39,6 +44,7 @@ const SideMenu = ({ cardTotal, cardRememberTotal, cardWriteTotal }) => (
 
             <Menu.Menu>
                 <MenuLink to="/user/statistics">
+                    <Icon type="stats-dots" />
                     Статистика
                 </MenuLink>
             </Menu.Menu>
@@ -57,7 +63,6 @@ const SideMenu = ({ cardTotal, cardRememberTotal, cardWriteTotal }) => (
             </Menu.Menu>
         </Menu.Item>
     </Menu>
-)
 
 SideMenu.propTypes = {
     cardTotal: PropTypes.number,
