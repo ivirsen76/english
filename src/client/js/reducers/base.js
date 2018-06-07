31bbdd097fd8f89f9289b443dceecee268bfbed8
Igor Eremeev <ivirsen@gmail.com>
2018-06-07 07:16:18 -0400

    Use ieremeev/axios instead of general one

--- src/client/js/reducers/base.js
+++ src/client/js/reducers/base.js
@@ -1,50 +1,50 @@
-import { handleActions, createAction } from 'redux-actions'
-import axios from 'axios'
+import { handleActions, createAction } from 'redux-actions';
+import axios from '@ieremeev/axios';
 
-export const minNewId = 1000000000
-export const maxWriteAttempts = 3
+export const minNewId = 1000000000;
+export const maxWriteAttempts = 3;
 
 // Initial state
 export const initialState = {
     loading: true,
     list: [],
-}
+};
 
 // Actions
-const ADD_BASE = 'english/base/ADD_BASE'
-const DELETE_BASE = 'english/base/DELETE_BASE'
-const UPDATE_BASE = 'english/base/UPDATE_BASE'
-const SET_LOADING_BASES_STATE = 'english/base/SET_LOADING_BASES_STATE'
-const SET_BASES = 'english/base/SET_BASES'
+const ADD_BASE = 'english/base/ADD_BASE';
+const DELETE_BASE = 'english/base/DELETE_BASE';
+const UPDATE_BASE = 'english/base/UPDATE_BASE';
+const SET_LOADING_BASES_STATE = 'english/base/SET_LOADING_BASES_STATE';
+const SET_BASES = 'english/base/SET_BASES';
 
 // Action Creators
-export const addBaseWithoutSaving = createAction(ADD_BASE)
-export const deleteBaseWithoutSaving = createAction(DELETE_BASE)
-export const updateBaseWithoutSaving = createAction(UPDATE_BASE)
-export const setLoadingBasesState = createAction(SET_LOADING_BASES_STATE)
-export const setBases = createAction(SET_BASES)
+export const addBaseWithoutSaving = createAction(ADD_BASE);
+export const deleteBaseWithoutSaving = createAction(DELETE_BASE);
+export const updateBaseWithoutSaving = createAction(UPDATE_BASE);
+export const setLoadingBasesState = createAction(SET_LOADING_BASES_STATE);
+export const setBases = createAction(SET_BASES);
 
 export const addBase = baseInfo => async (dispatch, getState) => {
-    const response = await axios.post('/bases', baseInfo)
-    dispatch(addBaseWithoutSaving(response.data))
-}
+    const response = await axios.post('/bases', baseInfo);
+    dispatch(addBaseWithoutSaving(response.data));
+};
 
 export const updateBase = baseInfo => async (dispatch, getState) => {
-    await axios.patch(`/bases/${baseInfo.id}`, baseInfo)
-    dispatch(updateBaseWithoutSaving(baseInfo))
-}
+    await axios.patch(`/bases/${baseInfo.id}`, baseInfo);
+    dispatch(updateBaseWithoutSaving(baseInfo));
+};
 
 export const deleteBase = baseId => async (dispatch, getState) => {
-    await axios.delete(`/bases/${baseId}`)
-    dispatch(deleteBaseWithoutSaving(baseId))
-}
+    await axios.delete(`/bases/${baseId}`);
+    dispatch(deleteBaseWithoutSaving(baseId));
+};
 
 export const loadBases = () => async (dispatch, getState) => {
-    dispatch(setLoadingBasesState(true))
-    const response = await axios.get('/bases')
-    dispatch(setBases(response.data.data))
-    dispatch(setLoadingBasesState(false))
-}
+    dispatch(setLoadingBasesState(true));
+    const response = await axios.get('/bases');
+    dispatch(setBases(response.data.data));
+    dispatch(setLoadingBasesState(false));
+};
 
 // Reducer
 export default handleActions(
@@ -64,10 +64,10 @@ export default handleActions(
                     return {
                         ...item,
                         ...action.payload,
-                    }
+                    };
                 }
 
-                return item
+                return item;
             }),
         }),
         [SET_BASES]: (state, action) => ({
@@ -80,4 +80,4 @@ export default handleActions(
         }),
     },
     initialState
-)
+);
