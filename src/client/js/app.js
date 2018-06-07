e5a0e8cd9598685f063de9db43825c4e4e9b9442
Igor Eremeev <ivirsen@gmail.com>
2018-06-07 07:40:25 -0400

    Save token in cookies

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,17 +1,24 @@
 // Import common global styles
-import '../styles/app.css'
+import '../styles/app.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App, createStore } from '@ieremeev/boilerplate'
-import reducers from './reducers.js'
-import Routes from './Routes.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App, createStore } from '@ieremeev/boilerplate';
+import reducers from './reducers.js';
+import Routes from './Routes.js';
+import cookie from 'js-cookie';
+import { authenticate } from 'js/reducers/auth';
 
-const store = createStore(reducers)
+const store = createStore(reducers);
+
+const token = cookie.get('token');
+if (token) {
+    store.dispatch(authenticate(token));
+}
 
 ReactDOM.render(
     <App store={store}>
         <Routes />
     </App>,
     document.getElementById('app')
-)
+);
--- src/client/js/components/NavBar/index.js
+++ src/client/js/components/NavBar/index.js
@@ -1,54 +1,68 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Dropdown, Menu, Modal } from 'semantic-ui-react'
-import MenuLink from '../MenuLink'
-import LoginForm from 'js/components/LoginForm'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dropdown, Menu, Modal } from 'semantic-ui-react';
+import MenuLink from '../MenuLink';
+import LoginForm from 'js/components/LoginForm';
+import { connect } from 'react-redux';
 
-const TopMenu = ({ isLoggedIn, user }) => (
-    <Menu inverted>
-        <MenuLink to="/" exact>
-            Home
-        </MenuLink>
-        <MenuLink to="/features">Features</MenuLink>
-        <Menu.Item>Card base</Menu.Item>
-        <Dropdown item text="Helpers">
-            <Dropdown.Menu>
-                <Dropdown.Item>Matrix Simple</Dropdown.Item>
-                <Dropdown.Item>Matrix Continuous</Dropdown.Item>
-                <Dropdown.Item>Numbers</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-        {isLoggedIn ? (
-            <Menu.Menu position="right">
-                <MenuLink to="/user">Dashboard</MenuLink>
-                <Dropdown
-                    item
-                    trigger={
-                        <span>
-                            <i className="icon-user" /> {user.email}
-                        </span>
-                    }
-                >
+class NavBar extends React.Component {
+    static propTypes = {
+        isLoggedIn: PropTypes.bool,
+        user: PropTypes.object,
+    };
+
+    render() {
+        const { isLoggedIn, user } = this.props;
+
+        return (
+            <Menu inverted>
+                <MenuLink to="/" exact>
+                    Home
+                </MenuLink>
+                <MenuLink to="/features">Features</MenuLink>
+                <Menu.Item>Card base</Menu.Item>
+                <Dropdown item text="Helpers">
                     <Dropdown.Menu>
-                        <Dropdown.Item>Change password</Dropdown.Item>
-                        <MenuLink to="/logout">Log out</MenuLink>
+                        <Dropdown.Item>Matrix Simple</Dropdown.Item>
+                        <Dropdown.Item>Matrix Continuous</Dropdown.Item>
+                        <Dropdown.Item>Numbers</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
-            </Menu.Menu>
-        ) : (
-            <Menu.Menu position="right">
-                <Modal size="small" closeIcon trigger={<a className="item">Log in</a>}>
-                    <Modal.Header>Log in</Modal.Header>
-                    <Modal.Content><LoginForm /></Modal.Content>
-                </Modal>
-            </Menu.Menu>
-        )}
-    </Menu>
-)
-
-TopMenu.propTypes = {
-    isLoggedIn: PropTypes.bool,
-    user: PropTypes.object,
+                {isLoggedIn ? (
+                    <Menu.Menu position="right">
+                        <MenuLink to="/user">Dashboard</MenuLink>
+                        <Dropdown
+                            item
+                            trigger={
+                                <span>
+                                    <i className="icon-user" /> {user.email}
+                                </span>
+                            }
+                        >
+                            <Dropdown.Menu>
+                                <Dropdown.Item>Change password</Dropdown.Item>
+                                <MenuLink to="/logout">Log out</MenuLink>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Menu>
+                ) : (
+                    <Menu.Menu position="right">
+                        <Modal size="small" closeIcon trigger={<a className="item">Log in</a>}>
+                            <Modal.Header>Log in</Modal.Header>
+                            <Modal.Content>
+                                <LoginForm />
+                            </Modal.Content>
+                        </Modal>
+                    </Menu.Menu>
+                )}
+            </Menu>
+        );
+    }
 }
 
-export default TopMenu
+const mapStateToProps = state => ({
+    isLoggedIn: !!state.app.auth.token,
+    user: state.app.auth.user,
+});
+
+export default connect(mapStateToProps)(NavBar);
