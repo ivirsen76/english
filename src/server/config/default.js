cf772327878c6bbc4b0ff37e54e02b80a98506da
Igor Eremeev <ivirsen@gmail.com>
2018-08-26 22:29:48 -0400

    Use env variables for db request

--- src/server/config/default.js
+++ src/server/config/default.js
@@ -1,13 +1,24 @@
+const {
+    RDS_DB_NAME = 'feather',
+    RDS_HOSTNAME = 'localhost',
+    RDS_USERNAME = 'root',
+    RDS_PASSWORD = 'root',
+    RDS_PORT = 3306,
+    HOST = 'localhost',
+    PORT = 303,
+    PUBLIC_PATH = '../../../public/',
+    ENGLISH_API_AUTH_TOKEN_SECRET = 'O1It4sjOS9iWMD4F1z3uQvZsYKXrI19YqqKrtAZWQ==',
+} = process.env
+
 module.exports = {
-    host: 'localhost',
-    port: 3030,
-    mysql: 'mysql://root:root@localhost:3306/feather',
-    public: '../../../public/',
+    host: HOST,
+    port: PORT,
+    mysql: `mysql://${RDS_USERNAME}:${RDS_PASSWORD}@${RDS_HOSTNAME}:${RDS_PORT}/${RDS_DB_NAME}`,
+    public: PUBLIC_PATH,
     auth: {
         idField: 'id',
         token: {
-            secret:
-                'EkM4Gh5d27p+pmE9WJbYWSexlDxDF+RM19AJUSx3RfbziO1It4sjOS9iWMD4F1z3uQvZsYKXrI19YqqKrtAZWQ==',
+            secret: ENGLISH_API_AUTH_TOKEN_SECRET,
         },
         local: {},
     },
