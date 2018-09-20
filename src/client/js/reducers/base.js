7d183fbc8efa464442065575fcf63843c2180815
Igor Eremeev <ivirsen@gmail.com>
2018-09-19 23:05:58 -0400

    Add moveElement action

--- src/client/js/reducers/base.js
+++ src/client/js/reducers/base.js
@@ -19,6 +19,7 @@ const ADD_CARD = 'english/base/ADD_CARD'
 const DELETE_CARD = 'english/base/DELETE_CARD'
 const UPDATE_CARD = 'english/base/UPDATE_CARD'
 const SET_CARDS_FOR_BASE = 'english/base/SET_CARDS_FOR_BASE'
+const MOVE_ELEMENT = 'english/base/MOVE_ELEMENT'
 
 // Action Creators
 export const addBaseWithoutSaving = createAction(ADD_BASE)
@@ -30,6 +31,7 @@ export const addCardWithoutSaving = createAction(ADD_CARD)
 export const deleteCardWithoutSaving = createAction(DELETE_CARD)
 export const updateCardWithoutSaving = createAction(UPDATE_CARD)
 export const setCardsForBase = createAction(SET_CARDS_FOR_BASE)
+export const moveElement = createAction(MOVE_ELEMENT)
 
 export const addBase = baseInfo => async (dispatch, getState) => {
     const response = await axios.post('/bases', baseInfo)
@@ -145,6 +147,49 @@ export default handleActions(
                     ...state.cards.filter(item => item.baseId !== baseId),
                     ...cards.map(item => ({ ...item, baseId })),
                 ],
+            }
+        },
+        [MOVE_ELEMENT]: (state, action) => {
+            const { id, parentId, beforeId = 0 } = action.payload
+
+            const element = state.list.find(item => item.id === id)
+            const beforeElement = state.list.find(item => item.id === beforeId)
+            let beforePosition
+            if (beforeElement) {
+                beforePosition = beforeElement.position
+            } else {
+                beforePosition = state.list.filter(item => item.parentId === parentId).length
+            }
+
+            const list = state.list.map(item => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        parentId,
+                        position: beforePosition,
+                    }
+                }
+
+                if (item.parentId === parentId && item.position >= beforePosition) {
+                    return {
+                        ...item,
+                        position: item.position + 1,
+                    }
+                }
+
+                if (item.parentId === element.parentId && item.position > element.position) {
+                    return {
+                        ...item,
+                        position: item.position - 1,
+                    }
+                }
+
+                return item
+            })
+
+            return {
+                ...state,
+                list,
             }
         },
     },
--- src/client/js/reducers/base.test.js
+++ src/client/js/reducers/base.test.js
@@ -9,144 +9,215 @@ import reducer, {
     setBases,
     setLoadingBasesState,
     setCardsForBase,
+    moveElement,
 } from './base'
 
-describe('base reducer', () => {
-    describe('setLoadingBasesState()', () => {
-        it('Should set loading bases state', () => {
-            const state = {
-                ...initialState,
-                loading: false,
-            }
-            expect(reducer(state, setLoadingBasesState(true)).loading).toBe(true)
-        })
+describe('setLoadingBasesState()', () => {
+    it('Should set loading bases state', () => {
+        const state = {
+            ...initialState,
+            loading: false,
+        }
+        expect(reducer(state, setLoadingBasesState(true)).loading).toBe(true)
     })
+})
 
-    describe('addBase()', () => {
-        it('Should add new base', () => {
-            const state = {
-                ...initialState,
-                list: [],
-            }
-            const expectedList = [{ id: 12, title: 'Tree', price: 800 }]
-            const resultedState = reducer(state, addBase({ id: 12, title: 'Tree', price: 800 }))
-            expect(resultedState.list).toEqual(expectedList)
-        })
+describe('addBase()', () => {
+    it('Should add new base', () => {
+        const state = {
+            ...initialState,
+            list: [],
+        }
+        const expectedList = [{ id: 12, title: 'Tree', price: 800 }]
+        const resultedState = reducer(state, addBase({ id: 12, title: 'Tree', price: 800 }))
+        expect(resultedState.list).toEqual(expectedList)
     })
+})
 
-    describe('deleteBase()', () => {
-        it('Should delete base and remove it from remember list', () => {
-            const state = {
-                ...initialState,
-                list: [
-                    { id: 1, title: 'Tree1', price: 800 },
-                    { id: 2, title: 'Tree2', price: 800 },
-                ],
-            }
-            const expectedList = [{ id: 2, title: 'Tree2', price: 800 }]
-            const resultedState = reducer(state, deleteBase(1))
-            expect(resultedState.list).toEqual(expectedList)
-        })
+describe('deleteBase()', () => {
+    it('Should delete base and remove it from remember list', () => {
+        const state = {
+            ...initialState,
+            list: [{ id: 1, title: 'Tree1', price: 800 }, { id: 2, title: 'Tree2', price: 800 }],
+        }
+        const expectedList = [{ id: 2, title: 'Tree2', price: 800 }]
+        const resultedState = reducer(state, deleteBase(1))
+        expect(resultedState.list).toEqual(expectedList)
     })
+})
 
-    describe('updateBase()', () => {
-        it('Should update base', () => {
-            const state = {
-                ...initialState,
-                list: [{ id: 2, title: 'Tree2', price: 800 }],
-            }
-            const expectedList = [{ id: 2, title: 'TreeUpdated', price: 800 }]
-            const resultedState = reducer(state, updateBase({ id: 2, title: 'TreeUpdated' }))
-            expect(resultedState.list).toEqual(expectedList)
-        })
+describe('updateBase()', () => {
+    it('Should update base', () => {
+        const state = {
+            ...initialState,
+            list: [{ id: 2, title: 'Tree2', price: 800 }],
+        }
+        const expectedList = [{ id: 2, title: 'TreeUpdated', price: 800 }]
+        const resultedState = reducer(state, updateBase({ id: 2, title: 'TreeUpdated' }))
+        expect(resultedState.list).toEqual(expectedList)
     })
+})
 
-    describe('setBases()', () => {
-        it('Should add bases', () => {
-            const state = {
-                ...initialState,
-                list: [{ id: 2, title: 'Tree2', price: 800 }],
-            }
-            const expectedList = [
-                { id: 3, title: 'Tree2', price: 800 },
-                { id: 4, title: 'Tree2', price: 800 },
-            ]
-            const resultedState = reducer(
-                state,
-                setBases([
-                    { id: 3, title: 'Tree2', price: 800 },
-                    { id: 4, title: 'Tree2', price: 800 },
-                ])
-            )
-            expect(resultedState.list).toEqual(expectedList)
-        })
+describe('setBases()', () => {
+    it('Should add bases', () => {
+        const state = {
+            ...initialState,
+            list: [{ id: 2, title: 'Tree2', price: 800 }],
+        }
+        const expectedList = [
+            { id: 3, title: 'Tree2', price: 800 },
+            { id: 4, title: 'Tree2', price: 800 },
+        ]
+        const resultedState = reducer(
+            state,
+            setBases([{ id: 3, title: 'Tree2', price: 800 }, { id: 4, title: 'Tree2', price: 800 }])
+        )
+        expect(resultedState.list).toEqual(expectedList)
     })
+})
 
-    describe('addCard()', () => {
-        it('Should add card to base', () => {
-            const oldCard = { id: 4, baseId: 2, text: 'More', translate: 'Anymore' }
-            const newCard = { id: 5, baseId: 1, text: 'Some', translate: 'Another' }
-            const state = {
-                ...initialState,
-                cards: [oldCard],
-            }
+describe('addCard()', () => {
+    it('Should add card to base', () => {
+        const oldCard = { id: 4, baseId: 2, text: 'More', translate: 'Anymore' }
+        const newCard = { id: 5, baseId: 1, text: 'Some', translate: 'Another' }
+        const state = {
+            ...initialState,
+            cards: [oldCard],
+        }
 
-            const resultedState = reducer(state, addCard(newCard))
-            expect(resultedState.cards).toEqual([oldCard, newCard])
-        })
+        const resultedState = reducer(state, addCard(newCard))
+        expect(resultedState.cards).toEqual([oldCard, newCard])
     })
+})
 
-    describe('deleteCard()', () => {
-        it('Should delete card', () => {
-            const state = {
-                ...initialState,
-                cards: [{ id: 1, text: 'Tree1' }, { id: 2, text: 'Tree2' }],
-            }
-            const expectedCards = [{ id: 2, text: 'Tree2' }]
-            const resultedState = reducer(state, deleteCard(1))
-            expect(resultedState.cards).toEqual(expectedCards)
-        })
+describe('deleteCard()', () => {
+    it('Should delete card', () => {
+        const state = {
+            ...initialState,
+            cards: [{ id: 1, text: 'Tree1' }, { id: 2, text: 'Tree2' }],
+        }
+        const expectedCards = [{ id: 2, text: 'Tree2' }]
+        const resultedState = reducer(state, deleteCard(1))
+        expect(resultedState.cards).toEqual(expectedCards)
     })
+})
 
-    describe('updateCard()', () => {
-        it('Should update card', () => {
-            const state = {
-                ...initialState,
-                cards: [
-                    { id: 3, text: 'One', translate: 'Another', baseId: 2 },
-                    { id: 4, text: 'More', translate: 'Anymore', baseId: 2 },
-                ],
-            }
-            const expectedCards = [
-                { id: 3, text: 'One', translate: 'Another', baseId: 2 },
-                { id: 4, text: 'Some', translate: 'Moreover', baseId: 2 },
-            ]
-
-            const resultedState = reducer(
-                state,
-                updateCard({ id: 4, text: 'Some', translate: 'Moreover' })
-            )
-            expect(resultedState.cards).toEqual(expectedCards)
-        })
-    })
-
-    describe('setCardsForBase()', () => {
-        it('Should set cards for base', () => {
-            const state = {
-                ...initialState,
-                cards: [{ id: 5, baseId: 2, text: 'Old', translate: 'ReallyOld' }],
-            }
-            const cards = [
-                { id: 3, text: 'One', translate: 'Another' },
-                { id: 4, text: 'More', translate: 'Anymore' },
-            ]
-            const expectedCards = [
+describe('updateCard()', () => {
+    it('Should update card', () => {
+        const state = {
+            ...initialState,
+            cards: [
                 { id: 3, text: 'One', translate: 'Another', baseId: 2 },
                 { id: 4, text: 'More', translate: 'Anymore', baseId: 2 },
-            ]
+            ],
+        }
+        const expectedCards = [
+            { id: 3, text: 'One', translate: 'Another', baseId: 2 },
+            { id: 4, text: 'Some', translate: 'Moreover', baseId: 2 },
+        ]
 
-            const resultedState = reducer(state, setCardsForBase({ baseId: 2, cards }))
-            expect(resultedState.cards).toEqual(expectedCards)
-        })
+        const resultedState = reducer(
+            state,
+            updateCard({ id: 4, text: 'Some', translate: 'Moreover' })
+        )
+        expect(resultedState.cards).toEqual(expectedCards)
+    })
+})
+
+describe('setCardsForBase()', () => {
+    it('Should set cards for base', () => {
+        const state = {
+            ...initialState,
+            cards: [{ id: 5, baseId: 2, text: 'Old', translate: 'ReallyOld' }],
+        }
+        const cards = [
+            { id: 3, text: 'One', translate: 'Another' },
+            { id: 4, text: 'More', translate: 'Anymore' },
+        ]
+        const expectedCards = [
+            { id: 3, text: 'One', translate: 'Another', baseId: 2 },
+            { id: 4, text: 'More', translate: 'Anymore', baseId: 2 },
+        ]
+
+        const resultedState = reducer(state, setCardsForBase({ baseId: 2, cards }))
+        expect(resultedState.cards).toEqual(expectedCards)
+    })
+})
+
+describe('moveElement()', () => {
+    const state = {
+        list: [
+            { id: 1, parentId: 0, position: 0 },
+            { id: 2, parentId: 1, position: 0 },
+            { id: 3, parentId: 1, position: 1 },
+            { id: 4, parentId: 0, position: 1 },
+            { id: 5, parentId: 4, position: 0 },
+            { id: 6, parentId: 4, position: 1 },
+        ],
+    }
+
+    it('Should move element in the same subtree', () => {
+        const expectedState = {
+            list: [
+                { id: 1, parentId: 0, position: 0 },
+                { id: 2, parentId: 1, position: 1 },
+                { id: 3, parentId: 1, position: 0 },
+                { id: 4, parentId: 0, position: 1 },
+                { id: 5, parentId: 4, position: 0 },
+                { id: 6, parentId: 4, position: 1 },
+            ],
+        }
+        const resultedState = reducer(
+            state,
+            moveElement({
+                id: 3,
+                parentId: 1,
+                beforeId: 2,
+            })
+        )
+        expect(resultedState).toEqual(expectedState)
+    })
+
+    it('Should move element to the different subtree', () => {
+        const expectedState = {
+            list: [
+                { id: 1, parentId: 0, position: 0 },
+                { id: 2, parentId: 1, position: 0 },
+                { id: 3, parentId: 1, position: 2 },
+                { id: 4, parentId: 0, position: 1 },
+                { id: 5, parentId: 1, position: 1 },
+                { id: 6, parentId: 4, position: 0 },
+            ],
+        }
+        const resultedState = reducer(
+            state,
+            moveElement({
+                id: 5,
+                parentId: 1,
+                beforeId: 3,
+            })
+        )
+        expect(resultedState).toEqual(expectedState)
+    })
+
+    it('Should move element to the end of the list', () => {
+        const expectedState = {
+            list: [
+                { id: 1, parentId: 0, position: 0 },
+                { id: 2, parentId: 4, position: 2 },
+                { id: 3, parentId: 1, position: 0 },
+                { id: 4, parentId: 0, position: 1 },
+                { id: 5, parentId: 4, position: 0 },
+                { id: 6, parentId: 4, position: 1 },
+            ],
+        }
+        const resultedState = reducer(
+            state,
+            moveElement({
+                id: 2,
+                parentId: 4,
+            })
+        )
+        expect(resultedState).toEqual(expectedState)
     })
 })
