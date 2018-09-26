2d562b1648ad515955752c8d7ac94c5540262865
Igor Eremeev <ivirsen@gmail.com>
2018-09-25 20:46:34 -0400

    Use @ieremeev/formik package

--- src/client/js/pages/Base/FolderView/index.js
+++ src/client/js/pages/Base/FolderView/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Formik, Field } from 'formik'
-import FormikInput from 'client/js/components/FormikInput'
-import Sync from './Sync.js'
+import { Formik, Field, SemanticInput } from '@ieremeev/formik'
 
 export const errorMessages = {
     noTitle: 'You have to provide title',
@@ -34,10 +32,10 @@ export default class Component extends React.Component {
                 initialValues={{ title: this.props.base.title }}
                 isInitialValid
                 validate={validate}
+                onValidChange={this.updateBase}
                 render={props => (
                     <form className="ui form">
-                        <Sync {...props} update={this.updateBase} />
-                        <Field name="title" component={FormikInput} label="Title" autoFocus />
+                        <Field name="title" component={SemanticInput} label="Title" autoFocus />
                     </form>
                 )}
             />
