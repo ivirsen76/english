2333de3f178b9b6855ccf30253042ba3a7f2025b
Igor Eremeev <ivirsen@gmail.com>
2018-09-28 14:51:43 -0400

    Test basetree API

---
+++ src/server/services/index.test.js
@@ -0,0 +1,90 @@
+// Testing rest API
+const app = require('../app')
+const supertest = require('supertest')
+const { restoreDb } = require('../../testcafe/db/utils.js')
+
+let server
+let request
+
+beforeAll(() => {
+    server = app.listen(5000)
+    request = supertest(server)
+    return new Promise(resolve => server.on('listening', resolve))
+})
+
+afterAll(() => {
+    restoreDb()
+    app.get('sequelize').close()
+    server.close()
+})
+
+beforeEach(() => {
+    restoreDb()
+})
+
+const loginAsStudent = async () => {
+    const result = await request.post('/auth/local').send({
+        email: 'student@gmail.com',
+        password: 'password',
+    })
+
+    return result.body.token
+}
+
+const loginAsAdmin = async () => {
+    const result = await request.post('/auth/local').send({
+        email: 'admin@gmail.com',
+        password: 'password',
+    })
+
+    return result.body.token
+}
+
+describe('base', () => {
+    describe('find', () => {
+        it('should return 401 for not logged in', async () => {
+            await request.get('/bases').expect(401)
+        })
+
+        it('should return 403 for student role', async () => {
+            const token = await loginAsStudent()
+            await request
+                .get('/bases')
+                .set('Authorization', token)
+                .expect(403)
+        })
+
+        it('should return 200 for admin role', async () => {
+            const token = await loginAsAdmin()
+            await request
+                .get('/bases')
+                .set('Authorization', token)
+                .expect(200)
+        })
+    })
+})
+
+describe('basetree', () => {
+    describe('create', () => {
+        it('should return 401 for not logged in', async () => {
+            await request.post('/basetree').expect(401)
+        })
+
+        it('should return 403 for student role', async () => {
+            const token = await loginAsStudent()
+            await request
+                .post('/basetree')
+                .set('Authorization', token)
+                .expect(403)
+        })
+
+        it('should return 201 for admin role', async () => {
+            const token = await loginAsAdmin()
+            await request
+                .post('/basetree')
+                .send([])
+                .set('Authorization', token)
+                .expect(201)
+        })
+    })
+})
