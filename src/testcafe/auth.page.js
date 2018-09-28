c8bbc6626e13358ebf398e4f90112fef2b9decd5
Igor Eremeev <ivirsen@gmail.com>
2018-09-28 10:34:16 -0400

    Add auth test

---
+++ src/testcafe/auth.page.js
@@ -0,0 +1,44 @@
+import { Selector } from 'testcafe'
+import { restoreDb } from './db/utils.js'
+import { url } from './config.js'
+
+fixture('Login and Logout').beforeEach(async t => {
+    restoreDb()
+})
+
+// Selectors
+const Body = Selector('body')
+const LoginLink = Selector('a').withText('Log in')
+const LoginSubmitButton = Selector('button').withText('Log in')
+const LogoutLink = Selector('a').withText('Log out')
+const UserMenuLink = Selector('span').withText('admin@gmail.com')
+
+test('Should log in and log out using menu options', async t => {
+    await t.navigateTo(url('/'))
+    await t.expect(Body.innerText).notContains('admin@gmail.com')
+
+    await t.click(LoginLink)
+    await t.typeText('input[name=email]', 'admin@gmail.com', { paste: true })
+    await t.typeText('input[name=password]', 'password', { paste: true })
+    await t.click(LoginSubmitButton)
+
+    await t.expect(Body.innerText).contains('admin@gmail.com')
+
+    await t.click(UserMenuLink)
+    await t.click(LogoutLink)
+    await t.expect(Body.innerText).contains('Log in')
+})
+
+test('Should log in and log out using pages', async t => {
+    await t.navigateTo(url('/login'))
+    await t.expect(Body.innerText).notContains('admin@gmail.com')
+
+    await t.typeText('input[name=email]', 'admin@gmail.com', { paste: true })
+    await t.typeText('input[name=password]', 'password', { paste: true })
+    await t.click(LoginSubmitButton)
+
+    await t.expect(Body.innerText).contains('admin@gmail.com')
+
+    await t.navigateTo(url('/logout'))
+    await t.expect(Body.innerText).contains('Log in')
+})
