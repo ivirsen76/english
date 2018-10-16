da3b0e0571aece0536c7f781a8aa3313d83aa674
Igor Eremeev <ivirsen@gmail.com>
2018-10-15 20:39:42 -0400

    Add studentUser role

--- src/testcafe/roles.js
+++ src/testcafe/roles.js
@@ -7,3 +7,10 @@ export const adminUser = Role(url('/login'), async t => {
         .typeText('input[name=password]', 'password', { paste: true })
         .click('button')
 })
+
+export const studentUser = Role(url('/login'), async t => {
+    await t
+        .typeText('input[name=email]', 'student@gmail.com', { paste: true })
+        .typeText('input[name=password]', 'password', { paste: true })
+        .click('button')
+})
