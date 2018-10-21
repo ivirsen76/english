7427d91ac47669394b40104a1a67fc6213b2c6ad
Igor Eremeev <ivirsen@gmail.com>
2018-10-21 13:37:54 -0400

    Simplify shipit config

--- shipitfile.js
+++ shipitfile.js
@@ -1,44 +1,16 @@
 /* eslint-disable global-require */
-const path = require('path')
-
 module.exports = shipit => {
     // Load shipit-deploy tasks
     require('shipit-deploy')(shipit)
-    const utils = require('shipit-utils')
 
     shipit.initConfig({
         default: {
-            repositoryUrl: 'git@github.com:ivirsen76/components.git',
-            dirToCopy: 'shipit',
+            repositoryUrl: 'git@github.com:ivirsen76/english.git',
             keepReleases: 3,
         },
         production: {
-            deployTo: '/var/www/demo.igor-eremeev.com',
-            servers: 'admin@185.72.246.95',
+            deployTo: '/home/admin/sites/word-word',
+            servers: 'admin@185.72.246.73',
         },
     })
-
-    shipit.blTask('cleanReleaseFolder', async () => {
-        await shipit.remote(`mv ${shipit.releasePath}/REVISION ${shipit.releasePath}/.REVISION`)
-        await shipit.remote(`rm -rf ${shipit.releasePath}/*`)
-        return await shipit.remote(
-            `mv ${shipit.releasePath}/.REVISION ${shipit.releasePath}/REVISION`
-        )
-    })
-
-    shipit.blTask('copyBuildFolder', async () => {
-        const buildDirectory = path.join(__dirname, 'build')
-        return await shipit.remoteCopy(buildDirectory, shipit.releasePath)
-    })
-
-    utils.registerTask(shipit, 'deploy', [
-        'deploy:init',
-        'deploy:fetch',
-        'deploy:update',
-        'cleanReleaseFolder',
-        'copyBuildFolder',
-        'deploy:publish',
-        'deploy:clean',
-        'deploy:finish',
-    ])
 }
