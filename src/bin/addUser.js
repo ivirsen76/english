8fc6c7f6eddded4e6bc3fa842e1019413fe431b4
Igor Eremeev <ivirsen@gmail.com>
2018-10-22 17:23:24 -0400

    Add addUser script

---
+++ src/bin/addUser.js
@@ -0,0 +1,38 @@
+const app = require('../server/app.js')
+const colors = require('colors/safe')
+const inquirer = require('inquirer')
+
+const getAnswers = () =>
+    inquirer.prompt([
+        {
+            name: 'email',
+            message: 'Email:',
+        },
+        {
+            name: 'password',
+            message: 'Password:',
+        },
+        {
+            name: 'roles',
+            type: 'list',
+            message: 'Role:',
+            choices: ['admin', 'student'],
+        },
+    ])
+
+const run = async () => {
+    const answers = await getAnswers()
+
+    try {
+        await app.service('users').create(answers)
+    } catch (e) {
+        const message = e.errors ? e.errors.map(item => item.message).join('\n') : e.message
+        console.error(colors.red(message))
+        process.exit(1)
+    }
+
+    console.info(colors.green('The user has been created'))
+    process.exit()
+}
+
+run()
