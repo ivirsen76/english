7b6b7246b5c93603c07126925d31514c368b8ffd
Igor Eremeev <ivirsen@gmail.com>
2018-10-23 19:21:56 -0400

    Send right headers

--- src/server/test.prod.js
+++ src/server/test.prod.js
@@ -16,7 +16,7 @@ describe('Pages', () => {
     it('should check some image', async () => {
         await request
             .get('/logo-3254c39614bc43d5f03eebf607256350.png')
-            .expect('Cache-Control', /max-age=604800/)
+            .expect('Cache-Control', /max-age=86400/)
             .expect('Content-Type', /image\/png/)
             .expect('X-Served-By', /^cache-/)
             .expect(200)
@@ -25,7 +25,7 @@ describe('Pages', () => {
     it('should check some sound', async () => {
         await request
             .get('/media/sample.mp3')
-            .expect('Cache-Control', /max-age=31536000/)
+            .expect('Cache-Control', /max-age=86400/)
             .expect('Content-Type', /image\/png/)
             .expect('X-Served-By', /^cache-/)
             .expect(200)
