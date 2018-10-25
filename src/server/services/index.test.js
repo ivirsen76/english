94b4876028d784335a7e57023451f0aeb13b5723
Igor Eremeev <ivirsen@gmail.com>
2018-10-25 13:45:39 -0400

    Use api prefix for service tests

--- src/server/services/index.test.js
+++ src/server/services/index.test.js
@@ -1,7 +1,12 @@
 // Testing rest API
 const app = require('../app')
 const supertest = require('supertest')
-const { restoreDb, getNumRecords, getRecord } = require('../../testcafe/db/utils.js')
+const {
+    restoreDb,
+    restoreSamples,
+    getNumRecords,
+    getRecord,
+} = require('../../testcafe/db/utils.js')
 
 let server
 let request
@@ -14,16 +19,18 @@ beforeAll(() => {
 
 afterAll(() => {
     restoreDb()
+    restoreSamples()
     app.get('sequelize').close()
     server.close()
 })
 
 beforeEach(() => {
     restoreDb()
+    restoreSamples()
 })
 
 const loginAsStudent = async () => {
-    const result = await request.post('/auth/local').send({
+    const result = await request.post('/api/auth/local').send({
         email: 'student@gmail.com',
         password: 'password',
     })
@@ -32,7 +39,7 @@ const loginAsStudent = async () => {
 }
 
 const loginAsAdmin = async () => {
-    const result = await request.post('/auth/local').send({
+    const result = await request.post('/api/auth/local').send({
         email: 'admin@gmail.com',
         password: 'password',
     })
@@ -43,20 +50,20 @@ const loginAsAdmin = async () => {
 describe('base', () => {
     describe('find', () => {
         it('should return 200 for not logged in', async () => {
-            await request.get('/bases').expect(200)
+            await request.get('/api/bases').expect(200)
         })
     })
 
     describe('get', () => {
         it('should return 405', async () => {
-            await request.get('/bases/1').expect(405)
+            await request.get('/api/bases/1').expect(405)
         })
     })
 
     describe('create', () => {
         it('should return 405', async () => {
             await request
-                .post('/bases')
+                .post('/api/bases')
                 .send({ title: 'some' })
                 .expect(405)
         })
@@ -64,19 +71,19 @@ describe('base', () => {
 
     describe('update', () => {
         it('should return 405', async () => {
-            await request.put('/bases/1').expect(405)
+            await request.put('/api/bases/1').expect(405)
         })
     })
 
     describe('patch', () => {
         it('should return 405', async () => {
-            await request.patch('/bases/1').expect(405)
+            await request.patch('/api/bases/1').expect(405)
         })
     })
 
     describe('delete', () => {
         it('should return 405', async () => {
-            await request.delete('/bases/1').expect(405)
+            await request.delete('/api/bases/1').expect(405)
         })
     })
 })
@@ -84,13 +91,13 @@ describe('base', () => {
 describe('basetree', () => {
     describe('create', () => {
         it('should return 401 for not logged in', async () => {
-            await request.post('/basetree').expect(401)
+            await request.post('/api/basetree').expect(401)
         })
 
         it('should return 403 for student role', async () => {
             const token = await loginAsStudent()
             await request
-                .post('/basetree')
+                .post('/api/basetree')
                 .set('Authorization', token)
                 .expect(403)
         })
@@ -100,26 +107,26 @@ describe('basetree', () => {
 describe('basecard', () => {
     describe('find', () => {
         it('should return 200 for not logged in student', async () => {
-            await request.get('/basecards').expect(200)
+            await request.get('/api/basecards').expect(200)
         })
     })
 
     describe('get', () => {
         it('should return 405', async () => {
-            await request.get('/basecards/1').expect(405)
+            await request.get('/api/basecards/1').expect(405)
         })
     })
 
     describe('update', () => {
         it('should return 405', async () => {
-            await request.put('/basecards/1').expect(405)
+            await request.put('/api/basecards/1').expect(405)
         })
     })
 
     describe('create', () => {
         it('should return 401 for not logged in', async () => {
             await request
-                .post('/basecards')
+                .post('/api/basecards')
                 .send({ text: 'new test one', translate: 'новая' })
                 .expect(401)
         })
@@ -127,7 +134,7 @@ describe('basecard', () => {
         it('should return 403 for student role', async () => {
             const token = await loginAsStudent()
             await request
-                .post('/basecards')
+                .post('/api/basecards')
                 .send({ text: 'new test one', translate: 'новая' })
                 .set('Authorization', token)
                 .expect(403)
@@ -138,7 +145,7 @@ describe('basecard', () => {
 
             const token = await loginAsAdmin()
             await request
-                .post('/basecards')
+                .post('/api/basecards')
                 .send({ text: 'new test one', translate: 'новая', baseId: 2 })
                 .set('Authorization', token)
                 .expect(201)
@@ -150,13 +157,13 @@ describe('basecard', () => {
 
     describe('patch', () => {
         it('should return 401 for not logged in', async () => {
-            await request.patch('/basecards/26').expect(401)
+            await request.patch('/api/basecards/26').expect(401)
         })
 
         it('should return 403 for student role', async () => {
             const token = await loginAsStudent()
             await request
-                .patch('/basecards/1')
+                .patch('/api/basecards/1')
                 .set('Authorization', token)
                 .expect(403)
         })
@@ -164,7 +171,7 @@ describe('basecard', () => {
         it('should return 200 for admin', async () => {
             const token = await loginAsAdmin()
             await request
-                .patch('/basecards/1')
+                .patch('/api/basecards/1')
                 .send({
                     text: 'bla-bla',
                     translate: 'сон',
@@ -178,13 +185,13 @@ describe('basecard', () => {
 
     describe('delete', () => {
         it('should return 401 for not logged in', async () => {
-            await request.delete('/basecards/1').expect(401)
+            await request.delete('/api/basecards/1').expect(401)
         })
 
         it('should return 403 for student', async () => {
             const token = await loginAsStudent()
             await request
-                .delete('/basecards/1')
+                .delete('/api/basecards/1')
                 .set('Authorization', token)
                 .expect(403)
         })
@@ -192,7 +199,7 @@ describe('basecard', () => {
         it('should return 200 for admin', async () => {
             const token = await loginAsAdmin()
             await request
-                .delete('/basecards/1')
+                .delete('/api/basecards/1')
                 .set('Authorization', token)
                 .expect(200)
         })
@@ -202,13 +209,13 @@ describe('basecard', () => {
 describe('cards', () => {
     describe('find', () => {
         it('should return 401 for not logged in', async () => {
-            await request.get('/cards').expect(401)
+            await request.get('/api/cards').expect(401)
         })
 
         it('should return 200 for student role', async () => {
             const token = await loginAsStudent()
             await request
-                .get('/cards')
+                .get('/api/cards')
                 .set('Authorization', token)
                 .expect(200)
         })
@@ -216,13 +223,13 @@ describe('cards', () => {
 
     describe('get', () => {
         it('should return 401 for not logged in', async () => {
-            await request.get('/cards/26').expect(401)
+            await request.get('/api/cards/26').expect(401)
         })
 
         it('should return 405 when getting card', async () => {
             const token = await loginAsStudent()
             await request
-                .get('/cards/26')
+                .get('/api/cards/26')
                 .set('Authorization', token)
                 .expect(405)
         })
@@ -230,13 +237,13 @@ describe('cards', () => {
 
     describe('create', () => {
         it('should return 401 for not logged in', async () => {
-            await request.post('/cards').expect(401)
+            await request.post('/api/cards').expect(401)
         })
 
         it('should return 400 when there are errors during creating the card', async () => {
             const token = await loginAsStudent()
             await request
-                .post('/cards')
+                .post('/api/cards')
                 .send({ text: 'русский', translate: 'english', label: '' })
                 .set('Authorization', token)
                 .expect(400)
@@ -245,7 +252,7 @@ describe('cards', () => {
         it('should return 200 when creating the card', async () => {
             const token = await loginAsStudent()
             await request
-                .post('/cards')
+                .post('/api/cards')
                 .send({ text: 'new test one', translate: 'новая', label: '', userId: 1 })
                 .set('Authorization', token)
                 .expect(201)
@@ -258,13 +265,13 @@ describe('cards', () => {
 
     describe('update', () => {
         it('should return 401 for not logged in', async () => {
-            await request.put('/cards/26').expect(401)
+            await request.put('/api/cards/26').expect(401)
         })
 
         it('should return 405 for logged in user', async () => {
             const token = await loginAsStudent()
             await request
-                .put('/cards/26')
+                .put('/api/cards/26')
                 .set('Authorization', token)
                 .expect(405)
         })
@@ -272,13 +279,13 @@ describe('cards', () => {
 
     describe('patch', () => {
         it('should return 401 for not logged in', async () => {
-            await request.patch('/cards/25').expect(401)
+            await request.patch('/api/cards/25').expect(401)
         })
 
         it('should return 403 for patching different user card', async () => {
             const token = await loginAsStudent()
             await request
-                .patch('/cards/26')
+                .patch('/api/cards/26')
                 .set('Authorization', token)
                 .expect(403)
         })
@@ -286,7 +293,7 @@ describe('cards', () => {
         it('should return 200 for patching your card', async () => {
             const token = await loginAsStudent()
             await request
-                .patch('/cards/25')
+                .patch('/api/cards/25')
                 .send({
                     text: 'bla-bla',
                     translate: 'сон',
@@ -311,13 +318,13 @@ describe('cards', () => {
 
     describe('delete', () => {
         it('should return 401 for not logged in', async () => {
-            await request.delete('/cards/25').expect(401)
+            await request.delete('/api/cards/25').expect(401)
         })
 
         it('should return 403 for deleting different user card', async () => {
             const token = await loginAsStudent()
             await request
-                .delete('/cards/26')
+                .delete('/api/cards/26')
                 .set('Authorization', token)
                 .expect(403)
         })
@@ -325,7 +332,7 @@ describe('cards', () => {
         it('should return 200 for deleting your card', async () => {
             const token = await loginAsStudent()
             await request
-                .delete('/cards/25')
+                .delete('/api/cards/25')
                 .set('Authorization', token)
                 .expect(200)
 
@@ -344,7 +351,7 @@ describe('basetocards', () => {
             const beforeNum = await getNumRecords('cards', { userId })
             const token = await loginAsStudent()
             await request
-                .put(`/basetocard/${baseId}`)
+                .put(`/api/basetocard/${baseId}`)
                 .set('Authorization', token)
                 .expect(200)
 
