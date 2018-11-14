3c8594a55e41f34af560af3321b1f5798dbcbbf8
Igor Eremeev <ivirsen@gmail.com>
2018-11-13 20:56:16 -0500

    Check access control allow origin

--- src/server/test.prod.js
+++ src/server/test.prod.js
@@ -1,12 +1,14 @@
 // Testing production HTTP requests
 const supertest = require('supertest')
 
-const request = supertest('http://www.word-word.club')
+const baseUrl = 'http://www.word-word.club'
+const request = supertest(baseUrl)
 
 describe('Pages', () => {
     it('should check home page', async () => {
         await request
             .get('/')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect('Content-Type', /text\/html; charset=UTF-8/)
             .expect('X-Served-By', /^cache-/)
@@ -16,6 +18,7 @@ describe('Pages', () => {
     it('should check some image', async () => {
         await request
             .get('/logo-3254c39614bc43d5f03eebf607256350.png')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /max-age=604800/)
             .expect('Content-Type', /image\/png/)
             .expect('X-Served-By', /^cache-/)
@@ -25,6 +28,7 @@ describe('Pages', () => {
     it('should check some sound', async () => {
         await request
             .get('/media/sample.mp3')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /max-age=604800/)
             .expect('Content-Type', /audio\/mpeg/)
             .expect('X-Served-By', /^cache-/)
@@ -34,6 +38,7 @@ describe('Pages', () => {
     it('should check return 404 for non existing sound', async () => {
         await request
             .get('/media/nonexistingsample.mp3')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect(404)
     })
@@ -41,6 +46,7 @@ describe('Pages', () => {
     it('should check return 404 for non existing image', async () => {
         await request
             .get('/nonexistingimage.png')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect(404)
     })
@@ -48,6 +54,7 @@ describe('Pages', () => {
     it('should check right API call', async () => {
         await request
             .get('/api/bases')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect('Content-Type', /application\/json/)
             .expect('X-Served-By', /^cache-/)
@@ -57,6 +64,7 @@ describe('Pages', () => {
     it('should check wrong API call', async () => {
         await request
             .get('/somewrongurl')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect('Content-Type', /text\/html/)
             .expect('X-Served-By', /^cache-/)
@@ -64,10 +72,11 @@ describe('Pages', () => {
     })
 })
 
-describe('AWS', () => {
+describe('Media', () => {
     it('Should return 405', async () => {
         await request
             .get('/api/mp3/1')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect(405)
     })
@@ -76,6 +85,7 @@ describe('AWS', () => {
     it('Should return 200', async () => {
         await request
             .get('/api/mp3/3478')
+            .expect('Access-Control-Allow-Origin', baseUrl)
             .expect('Cache-Control', /private, no-store/)
             .expect(200)
     })
