ec1d379d4bc7d6b60474b238e3642c218698dfc9
Igor Eremeev <ivirsen@gmail.com>
2018-11-16 09:30:08 -0500

    Save label from the base

--- src/server/services/basetocard/basetocard.class.js
+++ src/server/services/basetocard/basetocard.class.js
@@ -12,16 +12,19 @@ class Service {
 
     async update(baseId, data, params) {
         const userId = params.user.id
+        const baseService = this.app.getService('bases')
         const cardService = this.app.getService('cards')
         const basecardService = this.app.getService('basecards')
+        const currentBase = (await baseService.get(baseId)).dataValues
 
         const existingTexts = (await cardService.find({ query: { userId } })).data.map(item =>
             stripBrackets(item.text)
         )
         const newCards = (await basecardService.find({ query: { baseId } })).data
             .filter(item => !existingTexts.includes(stripBrackets(item.text)))
-            .map(item =>
-                _pick(item, [
+            .map(item => ({
+                label: currentBase.label || '',
+                ..._pick(item, [
                     'text',
                     'translate',
                     'ukSoundFile',
@@ -30,10 +33,10 @@ class Service {
                     'usSoundLength',
                     'ruSoundFile',
                     'ruSoundLength',
-                ])
-            )
+                ]),
+            }))
 
-        await Promise.all(newCards.map(card => cardService.create({ ...card, label: '', userId })))
+        await Promise.all(newCards.map(card => cardService.create({ ...card, userId })))
 
         return Promise.resolve('OK')
     }
