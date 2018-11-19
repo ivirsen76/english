91a7022eef22ccd26658aa42f864a6eeeb7522fa
Igor Eremeev <ivirsen@gmail.com>
2018-11-18 22:52:14 -0500

    Remove filter warning

--- src/server/services/basetocard/basetocard.filters.js
+++ src/server/services/basetocard/basetocard.filters.js
@@ -1,7 +1,3 @@
-console.warn(
-    'You are using the default filter for the basetocard service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering'
-)
-
 module.exports = function(data, connection, hook) {
     return data
 }
--- src/server/services/basetree/basetree.filters.js
+++ src/server/services/basetree/basetree.filters.js
@@ -1,7 +1,3 @@
-console.warn(
-    'You are using the default filter for the basetree service. For more information about event filters see https://docs.feathersjs.com/api/events.html#event-filtering'
-)
-
 module.exports = function(data, connection, hook) {
     return data
 }
